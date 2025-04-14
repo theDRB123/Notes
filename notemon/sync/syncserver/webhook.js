ca4f253641ec8c531c7013d1715232adb9bcd70a
Dhruv Bhanushali <119250923+theDRB123@users.noreply.github.com>
2025-04-14 12:38:01 +0530

    Minor Fix

--- notemon/sync/syncserver/webhook.js
+++ notemon/sync/syncserver/webhook.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.WEBHOOK_PORT || 9000;
 
 app.use(bodyParser.json());
+app.use(express.json());
 
 app.post('/webhook', (req, res) => {
     console.log('Received webhook event:', req.body);
