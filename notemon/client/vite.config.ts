dc35a380892ba97bc4c1491490dab34dc048d95a
Dhruv Bhanushali <dhrbhanushali123@gmail.com>
2025-04-14 10:49:31 +0530

    minor fix

--- notemon/client/vite.config.ts
+++ notemon/client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+let SERVER_DOMAIN = process.env.VITE_SERVER_DOMAIN || 'http://localhost:8081';
 let HOST = process.env.VITE_HOST || 'localhost';
 let PORT = Number.parseInt(process.env.VITE_PORT || "8080");
 
@@ -16,5 +17,8 @@ export default defineConfig({
     strictPort: true,
     host: true,
     origin: 'http://' + HOST + ':' + PORT,
+    allowedHosts: [
+      SERVER_DOMAIN
+    ]
   }
 });
